d7c3aa390e1a5a731dc8084b4019306939557595
A_Z_Kornoha <67061778+kortone@users.noreply.github.com>
2022-06-23 02:43:26 +0900

    update version name

--- TownOfSuper/Main.cs
+++ TownOfSuper/Main.cs
@@ -11,7 +11,7 @@ namespace TownOfSuper
     public class TosPlugin : BasePlugin
     {
         public const String Id = "jp.reitou-mugicha.tonwofsuper";
-        public const String Version = "1.1.0";
+        public const String Version = "1.1.0-updateFix";
 
         public static ConfigEntry<bool>? debugTool { get; set; }
         public static ConfigEntry<string>? StereotypedText { get; set; }
