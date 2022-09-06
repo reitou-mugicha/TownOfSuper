f59ba389224f4e8f2e11697a94f53a47df7b63df
reitou-mugicha <kounoaoto1029@outlook.jp>
2022-09-06 19:13:49 +0900

    最新版抜け

--- TownOfSuper/Main.cs
+++ TownOfSuper/Main.cs
@@ -19,7 +19,7 @@ namespace TownOfSuper
     {
         #nullable enable
         public const String Id = "jp.reitou-mugicha.townofsuper";
-        public const String Version = "1.2.2";
+        public const String Version = "1.3.0";
 
         public static ConfigEntry<bool>? debugTool { get; set; }
         public static ConfigEntry<string>? StereotypedText { get; set; }
